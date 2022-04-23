bd381365414f6c6744c3cc6ce50af9d3a8f85f53
Zhihao Zhong <s3756809@student.rmit.edu.au>
2022-04-23 16:23:44 +0800

    testing 5

--- client/src/actions/UsersActions.js
+++ client/src/actions/UsersActions.js
@@ -7,7 +7,11 @@ const BASE_ROUTE = 'https://rhe5u2o28i.execute-api.us-east-1.amazonaws.com/dev/u
 async function sendPost(route, msg) {
     route = BASE_ROUTE;
     try{
-        let postRes = await axios.post(route, msg);
+        let postRes = await axios.post(route, msg, {headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Request-Methods": "POST", 
+            "Access-Control-Request-Headers": "Content-Type"
+        }});
         console.log(postRes);
         return postRes;
     }catch(error){
