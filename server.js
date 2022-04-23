a8c523222599f1a0354595da36f5ac5d57148d07
Zhihao Zhong <s3756809@student.rmit.edu.au>
2022-04-23 13:26:12 +0800

    adding cors

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express();
 const path = require("path");
 const PORT = 8000;
+const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
