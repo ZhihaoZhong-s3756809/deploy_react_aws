85efcb8ea1ebdb3399e61db898a672345877edbb
Zico Zhong <54311356+ZhihaoZhong-s3756809@users.noreply.github.com>
2022-04-24 19:35:11 +0800

    hard code survey

--- client/src/components/RMIT.js
+++ client/src/components/RMIT.js
@@ -12,42 +12,42 @@ const Rmit = () => {
     const uniname = cookie.load('uniName');
     cookie.save('courseName', 'Bachelor of Computer Science', {path:'/'});
 
-    var q1 = '';
-    var q2 = '';
-    var q3 = '';
-    var q4 = '';
-    var q5 = '';
+    var q1 = 'Are you satisfied with the teaching team?(0 for lowest, 5 for highest, applies to all questions followed)';
+    var q2 = 'Are you satisfied with the teaching materials?';
+    var q3 = 'Are you satisfied with the facilities in the school?';
+    var q4 = 'Are you satisfied with the service within the course?';
+    var q5 = 'Are you satisfied with the assignments/examinations within this course?';
 
-    getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q1')(dispatch => {
-        if(dispatch.type === SUCCESS){
-            q1 = dispatch.content;
-            cookie.save('q1', q1);
-        }
-    });
-    getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q2')(dispatch => {
-        if(dispatch.type === SUCCESS){
-            q2 = dispatch.content;
-            cookie.save('q2', q2);
-        }
-    });
-    getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q3')(dispatch => {
-        if(dispatch.type === SUCCESS){
-            q3 = dispatch.content;
-            cookie.save('q3', q3);
-        }
-    });
-    getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q4')(dispatch => {
-        if(dispatch.type === SUCCESS){
-            q4 = dispatch.content;
-            cookie.save('q4', q4);
-        }
-    });
-    getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q5')(dispatch => {
-        if(dispatch.type === SUCCESS){
-            q5 = dispatch.content;
-            cookie.save('q5', q5);
-        }
-    });
+//     getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q1')(dispatch => {
+//         if(dispatch.type === SUCCESS){
+//             q1 = dispatch.content;
+//             cookie.save('q1', q1);
+//         }
+//     });
+//     getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q2')(dispatch => {
+//         if(dispatch.type === SUCCESS){
+//             q2 = dispatch.content;
+//             cookie.save('q2', q2);
+//         }
+//     });
+//     getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q3')(dispatch => {
+//         if(dispatch.type === SUCCESS){
+//             q3 = dispatch.content;
+//             cookie.save('q3', q3);
+//         }
+//     });
+//     getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q4')(dispatch => {
+//         if(dispatch.type === SUCCESS){
+//             q4 = dispatch.content;
+//             cookie.save('q4', q4);
+//         }
+//     });
+//     getSurveyQuestion(uniname, 'Bachelor of Computer Science', 'q5')(dispatch => {
+//         if(dispatch.type === SUCCESS){
+//             q5 = dispatch.content;
+//             cookie.save('q5', q5);
+//         }
+//     });
 
     const submitHandler = (event) => {
         history('/home', {state: ''});
